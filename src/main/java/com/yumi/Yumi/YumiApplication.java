2af43c2a95756d362aab20598bd3f7eb7ae1c508
Deshi ttc <dtorre007@gmail.com>
2025-10-03 21:25:20 -0500

    Primer commit

---
+++ src/main/java/com/yumi/Yumi/YumiApplication.java
@@ -0,0 +1,13 @@
+package com.yumi.Yumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yumi/Yumi/YumiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yumi.Yumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
